0ec70c3270f32ae40250350eebccf2af25283525
rshiva <nadarshiva.r@gmail.com>
2023-04-01 19:02:17 -0700

    dependency added and scrape all episode

--- splitter.py
+++ splitter.py
@@ -37,13 +37,13 @@ def split_text(content, chunk_size=500):
 
 
 # Load the original CSV file into a DataFrame
-original = #'/Users/shiva/workspace/podcast_scraper/tim_ferriss_transcript/content-04-03-2023-13:08:00.csv'
+original = "file_name.csv" #'/Users/shiva/workspace/podcast_scraper/tim_ferriss_transcript/content-04-03-2023-13:08:00.csv'
 df = pd.read_csv(original)
 
 # Define the list of strings to search for
 # famous_episodes = ["#576","#599","#582","#599","#561","#568","#600","#614","#641","#562","#633","#606","#565","#583",
 # "#625","#620","#566","#593","#597","#616","#601","#594","#584","#585","#578""#621","#579","#628","#586","#567"]
-famous_episodes = ["#576","#593","#601"]
+# famous_episodes = ["#576","#593","#601"]
 pattern = r"#\d+"
 
 
@@ -59,8 +59,8 @@ matching_rows = pd.DataFrame(columns=['title', 'heading', 'content','tokens'])
 # Iterate over the rows of the DataFrame
 for i, row in df.iterrows():
   matches = re.findall(pattern, row["title"])
-  common_matches = set(matches).intersection(set(famous_episodes))
-  if common_matches:
+#   common_matches = set(matches).intersection(set(famous_episodes))
+  if matches:
       contents = split_text(row["content"])
       for content in contents:
         content = remove_newlines(content)
